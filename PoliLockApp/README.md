d1bab47a43aad04ab9e9ad1ace017c9b87a775e3
rodrigomarcolin <rodrigosalesmarcolin@gmail.com>
2024-03-26 03:11:34 -0300

    create MqttContext

--- PoliLockApp/README.md
+++ PoliLockApp/README.md
@@ -7,3 +7,4 @@ Aplicativo criado para interagir com o Projeto IOT do PoliLock.
 1. Instale a versão 18.15.0 do Node, ou simplesmente instale o nvm e execute na pasta PoliLockApp: `nvm use`
 1. Instale os pacotes: `npm install`
 1. Execute o comando Ionic para rodar o projeto: `ionic serve`
+1. Modifique o conteúdo do arquivo `.env-example`, colocando as informações necessárias para a conexão ao Broker MQTT, e renomeie-o para `.env`
---
+++ PoliLockApp/src/contexts/MqttContext.tsx
@@ -0,0 +1,53 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import mqtt, { IClientOptions } from "mqtt";
+
+interface MqttContextType {
+  client: mqtt.MqttClient | null;
+  publish: (topic: string, message: string) => void;
+}
+
+const MqttContext = createContext<MqttContextType>({
+  client: null,
+  publish: () => {},
+});
+
+export const useMqtt = () => useContext(MqttContext);
+
+export const MqttProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [client, setClient] = useState<mqtt.MqttClient | null>(null);
+  const port = process.env.REACT_APP_MQTT_PORT
+    ? parseInt(process.env.REACT_APP_MQTT_PORT)
+    : undefined;
+
+  useEffect(() => {
+    const url = process.env.REACT_APP_MQTT_BROKER_URL;
+    const options: IClientOptions = {
+      username: process.env.REACT_APP_MQTT_USERNAME,
+      password: process.env.REACT_APP_MQTT_PASSWORD,
+      port: port,
+    };
+
+    if (url) {
+      const mqttClient = mqtt.connect(url, options);
+      setClient(mqttClient);
+
+      return () => {
+        mqttClient.end();
+      };
+    }
+  }, []);
+
+  const publish = (topic: string, message: string) => {
+    if (client) {
+      client.publish(topic, message);
+    }
+  };
+
+  return (
+    <MqttContext.Provider value={{ client, publish }}>
+      {children}
+    </MqttContext.Provider>
+  );
+};
