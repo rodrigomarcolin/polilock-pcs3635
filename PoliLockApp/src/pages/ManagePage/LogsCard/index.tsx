47174e23d872fc9d6540c48966c38cc0ad3e8a0c
rodrigomarcolin <rodrigosalesmarcolin@gmail.com>
2024-04-02 16:41:28 -0300

    Logs do SUPA!!!

--- PoliLockApp/src/pages/ManagePage/LogsCard/index.tsx
+++ PoliLockApp/src/pages/ManagePage/LogsCard/index.tsx
@@ -1,48 +1,53 @@
 import React from "react";
 import {
+  IonButton,
   IonCard,
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
+  IonIcon,
+  IonInput,
 } from "@ionic/react";
 
 import { useEffect, useState } from "react";
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
-import { SUPABASE_URL, SUPABASE_API_KEY } from "../../../config";
+import { SUPABASE_URL, SUPABASE_API_KEY, getMqttTopic } from "../../../config";
 
 import LogsList from "./LogsList";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+import styles from "./LogsCard.module.css";
+import { useMqtt } from "../../../contexts/MqttContext";
+import { refresh } from "ionicons/icons";
 
 const LogsCard: React.FC = () => {
   const [supabase, setSupabase] = useState<SupabaseClient | undefined>();
   const [logs, setLogs] = useState<any>();
+  const { publish } = useMqtt();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const refreshLogs = async () => {
+    const response = await supabase?.from("logs").select();
+    const data = response?.data;
+    setLogs(data);
+  };
 
   useEffect(() => {
     const supabase = createClient(SUPABASE_URL, SUPABASE_API_KEY);
     setSupabase(supabase);
-
-    const getLogs = async () => {
-      setLogs([
-        {
-          message: "Error: Could not connect to database",
-          date: "2024-04-02T08:30:15Z",
-        },
-        {
-          message: "Warning: Disk space running low",
-          date: "2024-04-02T09:15:47Z",
-        },
-        {
-          message: "Info: User 'john_doe' logged in",
-          date: "2024-04-02T10:05:22Z",
-        },
-        {
-          message: "Error: Server crashed unexpectedly",
-          date: "2024-04-02T11:20:10Z",
-        },
-      ]);
-    };
-
-    getLogs();
+    refreshLogs();
   }, []);
+
+  const logsTopic = getMqttTopic("logs");
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    publish(logsTopic, data.log);
+  };
 
   return (
     <IonCard>
@@ -51,6 +56,26 @@ const LogsCard: React.FC = () => {
       </IonCardHeader>
 
       <LogsList logs={logs} />
+      <IonCardContent>
+        <IonButton
+          className="ion-margin-top"
+          type="submit"
+          onClick={() => refreshLogs()}
+          expand="full"
+        >
+          <IonIcon slot="start" icon={refresh}></IonIcon>
+          Recarregar Logs
+        </IonButton>
+      </IonCardContent>
+
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.input}>
+        <IonInput {...register("log")} placeholder="Log aqui" />
+        </div>
+        <IonButton className="ion-margin-top" type="submit" expand="full">
+        Publicar LOG
+        </IonButton>
+      </form> */}
     </IonCard>
   );
 };
